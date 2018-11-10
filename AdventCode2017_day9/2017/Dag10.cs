fb87329eddcf2424c93983ff7ba91264efee87ab
Bessel <besselb@yahoo.com>
2018-11-10 12:37:44 +0100

    day10a *

--- AdventCode2017_day9/2017/Dag10.cs
+++ AdventCode2017_day9/2017/Dag10.cs
@@ -8,81 +8,85 @@ namespace AdventCode2017
 {
     public class Dag10 : AdventBase, AdventInterface
     {
+        private static bool test = false;
+        private static int baseListLength = (test ? 5 : 256);
+        private string text = test ? "3, 4, 1, 5" : Resources.dag10_2017;
+
+        private int baseLengthList { get { return baseList.Count(); } }
+        private List<int> baseList = new List<int>();
+        private List<int> knotLengthList = new List<int>();
+        private int index = 0;
+        private int skip = 0;
+
+        private int remainingLengthToEndOfBaseList { get { return baseLengthList - index; } }
+        private int amountToBeTaken(int knotlength) { return knotlength > remainingLengthToEndOfBaseList ?  remainingLengthToEndOfBaseList : knotlength; }
+        private int amountToBeTakenFromStartOfBaseList(int knotlength) { var usedAtTheEnd = knotlength - remainingLengthToEndOfBaseList; return usedAtTheEnd < 0 ? 0 : usedAtTheEnd; }
+
         public Dag10()
         {
+            InitializeBaseList();
+            InitializeKnotList();
             Calculate();
             WriteDebugAnswers(this);
         }
 
-        public void Calculate()
+        private void InitializeBaseList()
         {
-            Test = true;
-            string text = Test ? "3, 4, 1, 5" : Resources.dag10_2017;
-
-            var baseList = new List<int>();
-            var index = 0;
-            var skip = 0;
-
-            for (int i = 0; i < (Test ? 5 : 256); i++)
+            for (int i = 0; i < baseListLength; i++)
             {
                 baseList.Add(i);
             }
+        }
 
-            var knotLengthList = (text.Split(',')).Select(c=> Int32.Parse(c));
+        private void InitializeKnotList()
+        {
+            knotLengthList = (text.Split(',')).Select(c => Int32.Parse(c)).ToList();
+        }
+
+        public void Calculate()
+        {
             foreach (var knotLength in knotLengthList)
             {
-                
-                var endListLength = baseList.Count() - index;
-                var takeFromBegin = 0;
-                var takeLengthStartFromIndex = knotLength;
-                var takeToIndex = index + knotLength;
-                var isStartedOverAgain = false;
-                var isInTheMiddle = false;
-                if (knotLength > endListLength) {
-                    isStartedOverAgain = true;
-                    takeLengthStartFromIndex = endListLength;
-                    takeFromBegin = knotLength - endListLength;
-                }
-                if (index > 0 && !isStartedOverAgain) {
-                    isInTheMiddle = true;
-                    index = index - 1;
-                }
-                var knotListFromIndex  = baseList.Skip(index).Take(takeLengthStartFromIndex).ToList();
-                var knotListFromBegin = baseList.Take(takeFromBegin).ToList();
+                var knotListFromIndex = baseList.Skip(index).Take(amountToBeTaken(knotLength)).ToList();
+                var knotListFromBegin = baseList.Take(amountToBeTakenFromStartOfBaseList(knotLength)).ToList();
 
-                var joinedList = knotListFromBegin.Concat(knotListFromIndex).ToList();
+                var joinedList = knotListFromIndex.Concat(knotListFromBegin).ToList();
                 joinedList.Reverse();
-                //place reverserdList back into position. When 
-                var restList = baseList.Where(c => !joinedList.Contains(c)).ToList();
-                if (isStartedOverAgain)
+
+                //Build new list
+                //When jumpedFromEnd
+                if (amountToBeTakenFromStartOfBaseList(knotLength) > 0)
                 {
-                    var beginPart = joinedList.Take(takeFromBegin);
-                    baseList = beginPart.Concat(restList).Concat(joinedList.Where(c => !beginPart.Contains(c))).ToList();
-                    //baseList = joinedList.Take(takeFromBegin).Concat(restList).Concat(joinedList.Skip(index).Take(takeLengthStartFromIndex)).ToList();
+                    var endPart = joinedList.Take(amountToBeTaken(knotLength)).ToList();
+                    var beginPart = joinedList.Where(c => !endPart.Contains(c)).Take(amountToBeTakenFromStartOfBaseList(knotLength)).ToList();
+                    var remainingUntouchedList = baseList.Where(c => !joinedList.Contains(c)).ToList();
+                    baseList = beginPart.Concat(remainingUntouchedList).Concat(endPart).ToList();
                 }
-                else if (isInTheMiddle){
-                    var beginPart = restList.Take(index);
-                    var endPart = restList.Skip(index + joinedList.Count() - 1);
-                    //var endPart = baseList.Where(c => !beginPart.Contains(c) && !restList.Contains(c)).ToList();
+                //When in the middle?
+                else
+                {
+                    var beginPart = baseList.Take(index).ToList();
+                    var endPart = baseList.Where(c => !beginPart.Contains(c) && !joinedList.Contains(c)).ToList();
                     baseList = beginPart.Concat(joinedList).Concat(endPart).ToList();
                 }
-                else {
-                    baseList = joinedList.Concat(restList).ToList();
-                }
                 
-                
-                
-                
-                index = index + knotLength + skip;
-                if (index > baseList.Count()) { index = index - baseList.Count(); }
-                skip++;
-                WriteDebug<string>(string.Join(",",baseList.ToArray()), this);
-
+                //CalculateNewIndex
+                UpdateIndexValue(knotLength);
+                WriteDebug<string>(string.Join(",", baseList.ToArray()), this);
             }
 
-            //Answer1 = score;
+
+            Answer1 = baseList.Take(1).First() * baseList.Skip(1).First();
             //Answer2 = garbageScore;
         }
+
+        private void UpdateIndexValue(int knotlength)
+        {
+            index = index + skip + knotlength;
+            if (index > baseLengthList) index = index - baseLengthList;
+            skip++;
+        }
+
 
     }
 }
