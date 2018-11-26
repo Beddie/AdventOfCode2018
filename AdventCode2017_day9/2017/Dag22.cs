ff2e7ba0014cffa405c9e01e92cb19b04454b40b
Bessel <besselb@yahoo.com>
2018-11-26 12:32:51 +0100

    Dag22 *

--- AdventCode2017_day9/2017/Dag22.cs
+++ AdventCode2017_day9/2017/Dag22.cs
@@ -1,300 +1,163 @@
 ﻿using AdventCode;
 using AdventCode.Properties;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventCode2017
 {
     public class Dag22 : AdventBase, AdventInterface
     {
-
         private bool test = false;
-        private string puzzleString = Resources.dag21_2017;
-        private int iterations = 5;
+        private string puzzleString = Resources.dag22_2017;
         public Dag22()
         {
             CalculateAnswerA();
-           // CalculateAnswerB();
+            CalculateAnswerB();
         }
 
-        //../.# => ##./#../...
-        //.#./..#/### => #..#/..../..../#..#
+        private byte[] startGrid;
 
-        private Dictionary<byte[], byte[]> rules = new Dictionary<byte[], byte[]>();
-        // "../.# => ##./#../...
-        //.#./..#/### => #..#/..../..../#..#"}
+        private enum Direction
+        {
+            Left = 1
+            , Right = 2
+            , Up = 3
+            , Down = 4
+        }
 
         public void CalculateAnswerA()
         {
-            //.#.
-            //..#
-            //###
-            //test = false;
             if (test)
             {
-                rules.Add(pixelStringToSquare("../.#"), pixelStringToSquare("##./#../..."));
-                rules.Add(pixelStringToSquare(".#./..#/###"), pixelStringToSquare("#..#/..../..../#..#"));
+                startGrid = pixelStringToSquare("..##.....");
             }
             else
             {
-                foreach (var rulestring in puzzleString.Split('\n'))
-                {
-                    var rule = rulestring.Replace(" ", "").Replace(">", "").Replace("\r", "").Split('=');
-                    rules.Add(pixelStringToSquare(rule[0]), pixelStringToSquare(rule[1]));
-                }
+                startGrid = pixelStringToSquare(puzzleString);
             }
 
-            var startGrid = pixelStringToSquare(".#...####");
-            var gridList = new List<byte[]>();
-            var newCreatedGrid = startGrid;
-            gridList.Add(startGrid);
-            for (int i = 0; i < (test ? 2 : iterations); i++)
-            {
-                var newGridList = new List<byte[]>();
-                foreach (var newsq in gridList)
-                {
-                    newGridList.Add(GetNewSquare(newsq));
-                }
-                newCreatedGrid = Create1Grid(newGridList);
-                gridList = CreatedNewSquares(newCreatedGrid);
-            }
-            Debug.WriteLine($"{newCreatedGrid.Where(c => c == (byte)1).Count()}");
-        }
+            //Build big Grid around startgrid
+            var gridMapStride = test ? 1000 : 1000;
+            var gridMap = new byte[gridMapStride * gridMapStride];
+            var gridMapMiddle = (int)(gridMapStride / 2d);
+            var startGridStride = (int)Math.Sqrt(startGrid.Length);
+            var startGridMiddle = ((startGridStride - 1) / 2);
+            var startPositionLeftX = gridMapMiddle - startGridMiddle;
+            var startPositionRightX = gridMapMiddle + startGridMiddle;
+            var startPositionUpperY = gridMapMiddle - startGridMiddle;
+            var startPositionLowerY = gridMapMiddle + startGridMiddle;
+            var position = new Virus(gridMapStride, gridMapMiddle + (gridMapMiddle * gridMapStride));
 
-        /// <summary>
-        /// Create 1 total grid (which is one / 1 byte[]) again, to be used in new evaluation
-        /// </summary>
-        /// <param name="squares"></param>
-        /// <returns></returns>
-        private byte[] Create1Grid(List<byte[]> squares)
-        {
-            var squareByteSize = squares.Sum(c => c.Length);
-            var square1Length = (int)Math.Sqrt(squares.First().Length);
-            var grid1Length = (int)Math.Sqrt(squareByteSize);
-            var rowCount = square1Length * grid1Length;
-            var newByte = new byte[squareByteSize];
-            int x = 0;
-            int y = 1;
-
-            foreach (var square in squares)
+            //Initialize startgrid into gridMap;
+            for (int y = 0; y < gridMapStride; y++)
             {
-                for (int vertical = 0; vertical < square1Length; vertical++)
+                for (int x = 0; x < gridMapStride; x++)
                 {
-                    for (int horizontalMove = 0; horizontalMove < square1Length; horizontalMove++)
+                    var gridPosition = x + y * gridMapStride;
+
+                    if ((y >= startPositionUpperY && y <= startPositionLowerY) && (x >= startPositionLeftX && x <= startPositionRightX))
                     {
-                        newByte[x + horizontalMove + (vertical * grid1Length)] = square[(horizontalMove + (vertical * square1Length))];
+                        gridMap[gridPosition] = startGrid[(x - startPositionLeftX) + ((y - startPositionUpperY) * startGridStride)];
+                    }
+                    else
+                    {
+                        gridMap[gridPosition] = 0;
                     }
                 }
-                if (((x + square1Length) % grid1Length) == 0) { x = rowCount * y++; } else { x += square1Length; };
             }
-            return newByte;
+
+            //Walk grid
+            for (int i = 0; i < 10000; i++)
+            {
+                position.nodeValue = gridMap[position.currentPostition];
+                gridMap[position.currentPostition] = position.NewNodeValue;
+                position.Move();
+            }
+            Print(gridMap, gridMapStride, gridMapMiddle, position);
+
+            Debug.WriteLine($"Number of nodes being infected: {position.countInfectionCaused}");
         }
 
-        ///TODO refactor DRY
-        /// <summary>
-        /// Create new squares based on the total grid
-        /// </summary>
-        /// <param name="newSquare"></param>
-        /// <returns></returns>
-        private List<byte[]> CreatedNewSquares(byte[] newSquare)
+        private class Virus
         {
-            var newGrid = new List<byte[]>();
-            var squareSize = 2;
-            var square1Length = (int)System.Math.Sqrt(newSquare.Length);
-            var rowCount = squareSize * square1Length;
-            //var testsQuareLength = (newSquare.Length / 2d) % squareSize;
-            if ((newSquare.Length / 2d) % squareSize == 0d)
+            public Virus(int Stride, int CurrentPostition)
             {
-                var squareAmount = newSquare.Length / 4;
-                var x = 0;
-                var y = 1;
-                for (int i = 0; i < squareAmount; i++)
-                {
-                    var new2by2Square = new byte[4];
-                    new2by2Square[0] = newSquare[x + 0];
-                    new2by2Square[1] = newSquare[x + 1];
-                    new2by2Square[2] = newSquare[x + 0 + square1Length];
-                    new2by2Square[3] = newSquare[x + 1 + square1Length];
-                    newGrid.Add(new2by2Square);
+                stride = Stride;
+                currentPostition = CurrentPostition;
+            }
+            public byte nodeValue { get; set; }
+            public int countInfectionCaused { get; set; }
+            public int currentPostition { get; set; }
 
-                    if ((x + squareSize) % square1Length == 0) { x = rowCount * y++; }
-                    else { x += squareSize; };
+            private Direction vector { get; set; } = Direction.Up;
+            private int stride { get; set; }
+            private Direction directionBasedOnValue { get { return isNotInfected ? Direction.Left : Direction.Right; } }
+            private bool isNotInfected => nodeValue == 0;
+
+            public void Move()
+            {
+                var newDirection = directionBasedOnValue;
+                switch (vector)
+                {
+                    case Direction.Left:
+                        if (newDirection == Direction.Left) { currentPostition = currentPostition + stride; vector = Direction.Down; }
+                        if (newDirection == Direction.Right) { currentPostition = currentPostition - stride; vector = Direction.Up; }
+                        break;
+                    case Direction.Right:
+                        if (newDirection == Direction.Left) { currentPostition = currentPostition - stride; vector = Direction.Up; }
+                        if (newDirection == Direction.Right) { currentPostition = currentPostition + stride; vector = Direction.Down; }
+                        break;
+                    case Direction.Up:
+                        if (newDirection == Direction.Left) { currentPostition--; vector = Direction.Left; }
+                        if (newDirection == Direction.Right) { currentPostition++; vector = Direction.Right; }
+                        break;
+                    case Direction.Down:
+                        if (newDirection == Direction.Left) { currentPostition++; vector = Direction.Right; }
+                        if (newDirection == Direction.Right) { currentPostition--; vector = Direction.Left; }
+                        break;
+                    default:
+                        break;
                 }
             }
-            else
-            {
-                squareSize = 3;
-                var squareAmount = newSquare.Length / 9;
-                rowCount = squareSize * square1Length;
-                var x = 0;
-                var y = 1;
-                for (int i = 0; i < squareAmount; i++)
-                {
-                    var new2by2Square = new byte[9];
-                    new2by2Square[0] = newSquare[x + 0];
-                    new2by2Square[1] = newSquare[x + 1];
-                    new2by2Square[2] = newSquare[x + 2];
-                    new2by2Square[3] = newSquare[x + 0 + square1Length];
-                    new2by2Square[4] = newSquare[x + 1 + square1Length];
-                    new2by2Square[5] = newSquare[x + 2 + square1Length];
-                    new2by2Square[6] = newSquare[x + 0 + square1Length * 2];
-                    new2by2Square[7] = newSquare[x + 1 + square1Length * 2];
-                    new2by2Square[8] = newSquare[x + 2 + square1Length * 2];
-                    newGrid.Add(new2by2Square);
 
-                    if ((x + squareSize) % square1Length == 0) { x = rowCount * y++; ; }
-                    else { x += squareSize; };
+            public byte NewNodeValue
+            {
+                get
+                {
+                    if (isNotInfected)
+                    {
+                        countInfectionCaused++;
+                        return 1;
+                    }
+                    else { return 0; }
                 }
             }
-            return newGrid;
         }
 
-        /// <summary>
-        /// Return compared value or if not found, return item1 = false
-        /// </summary>
-        /// <param name="square"></param>
-        /// <returns></returns>
-        private (bool, byte[]) isEqualCompare(byte[] square)
+        private static void Print(byte[] bitmapByte, int stride, int gridMapMiddle, Virus postition)
         {
-            foreach (var rule in rules)
+            var printstring = new StringBuilder();
+            for (int y = 0; y < stride; y++)
             {
-                if (rule.Key.SequenceEqual(square))
+                for (int x = 0; x < stride; x++)
                 {
-                    return (true, rule.Value);
+                    var position = x + y * stride;
+                    var value = bitmapByte[position];
+                    if (position == postition.currentPostition)
+                    {
+                        printstring.Remove(printstring.Length - 1, 1);
+                        printstring.Append(string.Format("[{0}]", value == 0 ? "." : "#"));
+                    }
+                    else
+                    {
+                        printstring.Append(string.Format("{0} ", value == 0 ? "." : "#"));
+                    }
                 }
+                printstring.AppendLine("");
             }
-            return (false, null);
-        }
-
-        /// <summary>
-        /// Rotate and flip to find a match and return new square
-        /// </summary>
-        /// <param name="square"></param>
-        /// <returns></returns>
-        private byte[] GetNewSquare(byte[] square)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                var isEqual = isEqualCompare(square);
-                if (isEqual.Item1) return isEqual.Item2;
-                Rotate(ref square);
-            }
-            FlipX(ref square);
-            for (int i = 0; i < 4; i++)
-            {
-                var isEqual = isEqualCompare(square);
-                if (isEqual.Item1) return isEqual.Item2;
-                Rotate(ref square);
-            }
-            FlipX(ref square);
-            FlipY(ref square);
-            for (int i = 0; i < 4; i++)
-            {
-                var isEqual = isEqualCompare(square);
-                if (isEqual.Item1) return isEqual.Item2;
-                Rotate(ref square);
-            }
-            return null;
-        }
-
-        /// <summary>
-        /// Flip squares X-as
-        /// </summary>
-        /// <param name="square"></param>
-        private void FlipX(ref byte[] square)
-        {
-            var rs = new byte[square.Length];
-
-            switch (square.Length)
-            {
-                case 9:
-                    rs[0] = square[2];
-                    rs[1] = square[1];
-                    rs[2] = square[0];
-                    rs[3] = square[5];
-                    rs[4] = square[4];
-                    rs[5] = square[3];
-                    rs[6] = square[8];
-                    rs[7] = square[7];
-                    rs[8] = square[6];
-                    break;
-                default:
-                    rs[0] = square[1];
-                    rs[1] = square[0];
-                    rs[2] = square[3];
-                    rs[3] = square[2];
-                    break;
-            }
-            square = rs;
-        }
-
-        /// <summary>
-        /// Flip squares Y-as
-        /// </summary>
-        /// <param name="square"></param>
-        private void FlipY(ref byte[] square)
-        {
-            var rs = new byte[square.Length];
-
-            switch (square.Length)
-            {
-                case 9:
-                    rs[0] = square[6];
-                    rs[1] = square[7];
-                    rs[2] = square[8];
-                    rs[3] = square[3];
-                    rs[4] = square[4];
-                    rs[5] = square[5];
-                    rs[6] = square[0];
-                    rs[7] = square[1];
-                    rs[8] = square[2];
-                    break;
-                default:
-                    rs[0] = square[2];
-                    rs[1] = square[3];
-                    rs[2] = square[0];
-                    rs[3] = square[1];
-                    break;
-            }
-            square = rs;
-        }
-
-        /// <summary>
-        /// Rotate Square
-        /// </summary>
-        /// <param name="square"></param>
-        private void Rotate(ref byte[] square)
-        {
-            var rs = new byte[square.Length];
-
-            switch (square.Length)
-            {
-                case 9:
-                    rs[0] = square[6];
-                    rs[1] = square[3];
-                    rs[2] = square[0];
-                    rs[3] = square[7];
-                    rs[4] = square[4];
-                    rs[5] = square[1];
-                    rs[6] = square[8];
-                    rs[7] = square[5];
-                    rs[8] = square[2];
-                    break;
-                default:
-                    rs[0] = square[2];
-                    rs[1] = square[0];
-                    rs[2] = square[3];
-                    rs[3] = square[1];
-                    break;
-            }
-            square = rs;
+            printstring.AppendLine();
+            Debug.Print(printstring.ToString());
         }
 
         /// <summary>
@@ -304,7 +167,7 @@ namespace AdventCode2017
         /// <returns></returns>
         private byte[] pixelStringToSquare(string pixels)
         {
-            pixels = pixels.Replace("/", "");
+            pixels = pixels.Replace(" ", "").Replace(">", "").Replace("\r", "").Replace("/", "").Replace("\n", "");
             var pixelbytes = new byte[pixels.Length];
             var i = 0;
             foreach (var chartic in pixels)
@@ -316,8 +179,7 @@ namespace AdventCode2017
 
         public void CalculateAnswerB()
         {
-            iterations = 18;
-            CalculateAnswerA();
+            //CalculateAnswerA();
         }
     }
 }
--- AdventCode2017_day9/Program.cs
+++ AdventCode2017_day9/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var day21 = new Dag21();
+            var day22 = new Dag22();
         }
 
     }
