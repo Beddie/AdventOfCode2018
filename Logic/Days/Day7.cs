93362760b42479713304c60caa9b0f2b2dd64cc8
Bessel <besselb@yahoo.com>
2018-12-08 17:44:59 +0100

    Day7 .*

--- Logic/Days/Day7.cs
+++ Logic/Days/Day7.cs
@@ -16,7 +16,7 @@ namespace Logic.Days
     {
         public Day7()
         {
-            Test = true;
+            //Test = true;
             PuzzleInput = Test ? Resources.Day7Example : Resources.Day7;
         }
 
@@ -24,20 +24,17 @@ namespace Logic.Days
         {
             return new string[] {
                 "CFMNLOAHRKPTWBJSYZVGUQXIDE",
-                ""
+                "971"
             };
         }
 
         private List<char> orderedInstructions = new List<char>();
         private List<char[]> instructions = new List<char[]>();
-        private HashSet<char> queue = new HashSet<char>();
-        //private List<char> availables = new List<char>();
 
         public string Part1()
         {
             instructions = PuzzleInput.Split(new[] { "\r\n" }, StringSplitOptions.None).Select(c => new char[2] { c.Skip(5).Take(1).First(), c.Skip(36).Take(1).First() }).ToList();
             var allChars = instructions.Select(c => c[0]).Union(instructions.Select(c => c[1])).Distinct().OrderBy(c => c).ToList();
-            var iteratingAvailables = allChars.Where(c => !instructions.Select(d => d[1]).Contains(c)).OrderBy(c => c).ToList();
 
             while (allChars.Count() > 0)
             {
@@ -66,72 +63,89 @@ namespace Logic.Days
             return true;
         }
 
-        private class Worker
+        #region Part 2
+
+        private class InstuctionLetter
         {
             public char Letter { get; set; }
-            public int Seconds { get; set; }
+            public int Seconds { get; set; } = 0;
+            public bool WorkerDone { get; set; }
+            public bool Available { get; set; }
+            public bool Done => WorkerDone && Available;
+            public int Order { get; set; }
+            public bool InProcess { get; set; }
         }
 
-        private List<Worker> workers { get; set; } = new List<Worker>() { new Worker(), new Worker()};
+        private class Worker
+        {
+            public InstuctionLetter InstuctionLetter = new InstuctionLetter() { Seconds = -1 };
+            public bool Working => InstuctionLetter.Seconds > 0;
+
+            public void ProcessAvailables(InstuctionLetter available)
+            {
+                InstuctionLetter = available;
+                InstuctionLetter.InProcess = true;
+            }
+
+            public void Work()
+            {
+                InstuctionLetter.Seconds -= (InstuctionLetter.Seconds > 0 ? 1 : 0);
+                if (InstuctionLetter.Seconds == 0)
+                {
+                    InstuctionLetter.WorkerDone = true;
+                    InstuctionLetter.InProcess = false;
+                }
+            }
+        }
+
+        private List<InstuctionLetter> allChars = new List<InstuctionLetter>();
+        private List<Worker> workers { get; set; } = Test ? new List<Worker>() { new Worker(), new Worker() } : new List<Worker>() { new Worker(), new Worker(), new Worker(), new Worker(), new Worker() };
 
         public string Part2()
         {
             instructions = PuzzleInput.Split(new[] { "\r\n" }, StringSplitOptions.None).Select(c => new char[2] { c.Skip(5).Take(1).First(), c.Skip(36).Take(1).First() }).ToList();
-            var allChars = instructions.Select(c => c[0]).Union(instructions.Select(c => c[1])).Distinct().OrderBy(c => c).Select((d, index) => new { thisChar = d, seconds = index + 1 }).ToList();
-            var startingInstructions = allChars.Where(c => !instructions.Where(d => d[1] == c.thisChar).Any()).Select(c=> c.thisChar).ToList();
-            var iteratingAvailables = instructions.Where(c => startingInstructions.Contains(c[0])).ToList();
+            allChars = instructions.Select(c => c[0]).Union(instructions.Select(c => c[1])).Distinct().OrderBy(c => c).Select((d) => new InstuctionLetter { Letter = d, Seconds = d - (Test ? 64 : 4) }).ToList();
             var seconds = 0;
 
-            while (orderedInstructions.Count < allChars.Count && workers.All(c => c.Seconds == 0))
+            while (allChars.Any(c => !c.Done))
             {
-                devideInstructies(iteratingAvailables.Select(c => c[1]));
-                foreach (var letter in iteratingAvailables)
+                //Set Available letter properties
+                SetAvailablePropetyToAllAvailableLetters();
+
+                foreach (var letter in allChars.OrderBy(o => o.Letter).Where(c => !c.Done && c.Available && !c.InProcess && !c.WorkerDone))
                 {
-                    var instructies = instructions.Where(c => c[0] == letter[0]).ToList();
-                        //seconds += letter.seconds;
-                        var avail = ProcessAvailablesPart2(instructies);
-                        if (avail)
-                        {
-                            break;
-                        }
+                    var worker = workers.Where(c => !c.Working).FirstOrDefault();
+                    if (worker != null) worker.ProcessAvailables(letter);
                 }
+
+                //Let the Elves do their work
+                workers.ForEach(c => c.Work());
+
+                if (Test)
+                {
+                    Debug.WriteLine(string.Format("{0} AFTER WORK    DONE:{1}    PROCESS:{2}", seconds, new string(allChars.Where(c => c.Done).OrderBy(c => c.Order).Select(c => c.Letter).ToArray()), new string(allChars.Where(c => c.InProcess).OrderBy(c => c.Order).Select(c => c.Letter).ToArray())));
+                    foreach (var myChar in allChars.OrderBy(o => o.Letter).Where(c => c.Done && c.Order == 0)) myChar.Order = allChars.Max(c => c.Order) + 1;
+                }
+                seconds++;
             }
-            var charOrder = new string(orderedInstructions.ToArray());
-            return charOrder;
+
+            return seconds.ToString();
         }
 
-        private void devideInstructies(IEnumerable<char> instructies)
+        private void SetAvailablePropetyToAllAvailableLetters()
         {
-            //var workedInstructies= new List<Worker>();
-            foreach (var instructie in instructies)
+            foreach (var chartic in allChars.OrderBy(o => o.Letter).Where(c => !c.Available))
             {
-                foreach (var worker in workers)
-                {
-                    if (worker.Seconds == 0) { 
-                        worker.Letter = instructie;
-                        worker.Seconds = instructie - 64;
-                        break;
-                    };
-                }
+                chartic.Available = instructions.Where(c => c[1] == chartic.Letter).Select(c => c[0]).All(c => allChars.OrderBy(o => o.Letter).Where(d => d.Letter == c).First().WorkerDone);
             }
         }
 
-        private bool ProcessAvailablesPart2(List<char[]> instructies)
-        {
-            //if (orderedInstructions.Contains(available)) return false;
-            //var anchestorAvailables = instructions.Where(c => c[1] == available).Select(c => c[0]).ToList();
-            //var isAvailable = anchestorAvailables.TrueForAll(c => orderedInstructions.Contains(c));
-            //if (!isAvailable) return false;
 
-            ////Is available!
-            ////if (!orderedInstructions.Contains(available))
-            //orderedInstructions.Add(available);
-            return true;
-        }
+        #endregion
 
         public string GetListName()
         {
-            return "Day 7";
+            return "Day 7: The Sum of Its Parts";
         }
 
         public int GetID()
@@ -140,4 +154,3 @@ namespace Logic.Days
         }
     }
 }
-
