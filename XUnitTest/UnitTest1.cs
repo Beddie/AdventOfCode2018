c86c98ec7853acff1f6bafc20f7c849efd1ef49b
Bessel <besselb@yahoo.com>
2018-12-14 21:57:06 +0100

    Day13 .*

--- XUnitTest/UnitTest1.cs
+++ XUnitTest/UnitTest1.cs
@@ -14,24 +14,18 @@ namespace XUnitTest
         {
             var averageRunTime = new HashSet<long>();
 
-            //Execute puzzle 100 times
-            for (int i = 0; i < 2; i++)
+            //Execute puzzle 30 times
+            for (int i = 0; i < 30; i++)
             {
                 var sw = new Stopwatch();
                 sw.Start();
                 var day = RenderDay.GetDay(13);
                 var check = day.Part1();
-                //var day = RenderDay.GetDay(2);
-                //var check = day.Part2();
                 if (check != day.Solution()[0]) throw new Exception("Niet get goede antwoord!");
-                //Assert.AreEqual(check, day.Solution()[1]);
                 sw.Stop();
                 averageRunTime.Add(sw.ElapsedMilliseconds);
-                //Debug.WriteLine($"Completed in {sw.ElapsedMilliseconds} ms");
             }
             Debug.WriteLine($"Average complete in {averageRunTime.Average()} ms");
-            //Assert.AreEqual(day.Part1(), day.GetSolutions()[0]);
-            //Assert.AreEqual(day.Part2(), day.GetSolutions()[1]);
         }
     }
 }
